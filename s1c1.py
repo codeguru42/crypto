978452d41252a211596fb760e53de9d495d791cf
codeguru <codeguru@users.sourceforge.net>
2016-11-06 12:54:26 -0700

    Add main() function

--- s1c1.py
+++ s1c1.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 import boltons.iterutils as iterutils
 
-hexString = raw_input()
-bytesList = []
+def main():
+  hexString = raw_input()
+  bytesList = []
 
-for hexByte in iterutils.chunked(hexString, 2):
-  bytesList.append(int(hexByte, 16))
+  for hexByte in iterutils.chunked(hexString, 2):
+    bytesList.append(int(hexByte, 16))
 
-print(bytesList)
+  print(bytesList)
+
+if (__name__ == "__main__"):
+  main()
