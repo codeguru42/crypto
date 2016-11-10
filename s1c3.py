54177b98aade807986e98d1c25b845f48d0a9a88
codeguru <codeguru@users.sourceforge.net>
2016-11-10 10:21:48 -0700

    Naive decryption testing if plaintext is all alphabetic

--- s1c3.py
+++ s1c3.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def main():
-  pass
+  msg = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+  cipher = bytes.fromhex(msg)
+  print("cipher: ", cipher)
+
+  for key in range(256):
+    plain = ''.join([chr(key ^ x) for x in cipher])
+    if plain.isalpha():
+      print(plain)
 
 if __name__ == "__main__":
   main()
