e0033c6283c534f5f7e72a4e88a37aac9e2cb0a8
codeguru <codeguru@users.sourceforge.net>
2016-11-10 10:06:03 -0700

    Implement cryptopals.xor()

--- cryptopals.py
+++ cryptopals.py
@@ -1,4 +1,13 @@
 import base64
+import binascii
 
 def hexToBase64(hexStr):
   return base64.b64encode(bytes.fromhex(hexStr))
+
+def xor(a, b):
+  xs = bytes.fromhex(a)
+  ys = bytes.fromhex(b)
+  c = bytearray()
+  for x, y in zip(xs, ys):
+    c.append(x ^ y)
+  return binascii.hexlify(c)
--- s1c2.py
+++ s1c2.py
@@ -6,7 +6,7 @@ class TestXor(unittest.TestCase):
   def testCase(self):
     a = "1c0111001f010100061a024b53535009181c"
     b = "686974207468652062756c6c277320657965"
-    expected = "746865206b696420646f6e277420706c6179"
+    expected = b"746865206b696420646f6e277420706c6179"
     actual = cryptopals.xor(a, b)
     self.assertEqual(expected, actual)
 
