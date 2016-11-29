3e4849216283de04f621f904e79591af56a62d62
Code Apprentice <codeguru42@gmail.com>
2016-11-29 13:39:21 -0700

    Define xorcrypt() function

--- s1c3.py
+++ s1c3.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+def xorcrypt(key, cipher):
+  return ''.join([chr(key ^ x) for x in cipher])
+
 def main():
   msg = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
   cipher = bytes.fromhex(msg)
   print("cipher: ", cipher)
 
   for key in range(256):
-    plain = ''.join([chr(key ^ x) for x in cipher])
+    plain = xorcrypt(key, cipher)
     if plain.isalpha():
       print(plain)
 
