5286f34eaf1ec8680ca336d226b73d1b2430e2d9
Code Apprentice <codeguru42@gmail.com>
2016-12-22 06:59:40 -0700

    Implement functions with collections module
    Implement alphaCount() with Counter and expectedCounts() with defaultdict.

--- cryptopals.py
+++ cryptopals.py
@@ -1,6 +1,7 @@
 import base64
 import binascii
 import string
+import collections
 
 def hexToBase64(hexStr):
   return base64.b64encode(bytes.fromhex(hexStr))
@@ -25,41 +26,39 @@ def asciiUpper(text):
       yield c.upper()
 
 def alphaCount(text):
-  counts = [0] * 256
-  for c in asciiUpper(text):
-    counts[ord(c)] += 1
-  return counts
+  return collections.Counter(text.upper())
 
 def dist(count1, count2):
-  diffs = map(lambda x, y: abs(x - y), count1, count2)
+  keys = set(count1.keys()) | set(count2.keys())
+  diffs = map(lambda x: abs(count1[x] - count2[x]), keys)
   return sum(diffs)
 
 def expectedCounts(total):
-  freqs = [0] * 256
-  freqs[ord('A')] = total *  8.167 / 100.0
-  freqs[ord('B')] = total *  1.492 / 100.0
-  freqs[ord('C')] = total *  2.782 / 100.0
-  freqs[ord('D')] = total *  4.253 / 100.0
-  freqs[ord('E')] = total * 12.702 / 100.0
-  freqs[ord('F')] = total *  2.228 / 100.0
-  freqs[ord('G')] = total *  2.015 / 100.0
-  freqs[ord('H')] = total *  6.094 / 100.0
-  freqs[ord('I')] = total *  6.966 / 100.0
-  freqs[ord('J')] = total *  0.153 / 100.0
-  freqs[ord('K')] = total *  0.772 / 100.0
-  freqs[ord('L')] = total *  4.025 / 100.0
-  freqs[ord('M')] = total *  2.406 / 100.0
-  freqs[ord('N')] = total *  6.749 / 100.0
-  freqs[ord('O')] = total *  7.507 / 100.0
-  freqs[ord('P')] = total *  1.929 / 100.0
-  freqs[ord('Q')] = total *  0.095 / 100.0
-  freqs[ord('R')] = total *  5.987 / 100.0
-  freqs[ord('S')] = total *  6.327 / 100.0
-  freqs[ord('T')] = total *  9.056 / 100.0
-  freqs[ord('U')] = total *  2.758 / 100.0
-  freqs[ord('V')] = total *  0.978 / 100.0
-  freqs[ord('W')] = total *  2.360 / 100.0
-  freqs[ord('X')] = total *  0.150 / 100.0
-  freqs[ord('Y')] = total *  1.974 / 100.0
-  freqs[ord('Z')] = total *  0.074 / 100.0
+  freqs = collections.defaultdict(lambda : 0)
+  freqs['A'] = total *  8.167 / 100.0
+  freqs['B'] = total *  1.492 / 100.0
+  freqs['C'] = total *  2.782 / 100.0
+  freqs['D'] = total *  4.253 / 100.0
+  freqs['E'] = total * 12.702 / 100.0
+  freqs['F'] = total *  2.228 / 100.0
+  freqs['G'] = total *  2.015 / 100.0
+  freqs['H'] = total *  6.094 / 100.0
+  freqs['I'] = total *  6.966 / 100.0
+  freqs['J'] = total *  0.153 / 100.0
+  freqs['K'] = total *  0.772 / 100.0
+  freqs['L'] = total *  4.025 / 100.0
+  freqs['M'] = total *  2.406 / 100.0
+  freqs['N'] = total *  6.749 / 100.0
+  freqs['O'] = total *  7.507 / 100.0
+  freqs['P'] = total *  1.929 / 100.0
+  freqs['Q'] = total *  0.095 / 100.0
+  freqs['R'] = total *  5.987 / 100.0
+  freqs['S'] = total *  6.327 / 100.0
+  freqs['T'] = total *  9.056 / 100.0
+  freqs['U'] = total *  2.758 / 100.0
+  freqs['V'] = total *  0.978 / 100.0
+  freqs['W'] = total *  2.360 / 100.0
+  freqs['X'] = total *  0.150 / 100.0
+  freqs['Y'] = total *  1.974 / 100.0
+  freqs['Z'] = total *  0.074 / 100.0
   return freqs
