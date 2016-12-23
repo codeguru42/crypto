88f66469aafa81567731b07f69a32910423a8478
Code Apprentice <codeguru42@gmail.com>
2016-12-22 23:12:58 -0700

    Add s1c2 to alltests

--- alltests.py
+++ alltests.py
@@ -1,10 +1,12 @@
 import unittest
 import s1c1
+import s1c2
 
 def suite():
   suite = unittest.TestSuite()
   loader = unittest.TestLoader()
   suite.addTest(loader.loadTestsFromTestCase(s1c1.TestHexToBase64))
+  suite.addTest(loader.loadTestsFromTestCase(s1c2.TestXor))
 
   return suite
 
