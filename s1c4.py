c94a426a4fe3be09ecbf33aaa504d1648e38baa8
Code Apprentice <codeguru42@gmail.com>
2016-12-25 11:31:12 -0700

    Set 1 Challenge 4: Break encryption

--- s1c4.py
+++ s1c4.py
@@ -4,9 +4,9 @@ import cryptopals
 
 def main():
   with open(sys.argv[1]) as file:
-    for line in file:
-      b = bytes.fromhex(line.strip())
-      print(cryptopals.break_xor(b))
+    plaintexts = map(lambda line : cryptopals.break_xor(bytes.fromhex(line.strip())), file)
+    best = min(plaintexts, key=cryptopals.distance_from_english)
+    print(best)
 
 if __name__ == "__main__":
   main()
