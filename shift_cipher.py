4c7965c178a458ecbeb92b649d5931af68bbfbae
Code Apprentice <codeguru42@gmail.com>
2016-12-25 12:34:02 -0700

    Shift Cipher: Add positonal argument for key

--- shift_cipher.py
+++ shift_cipher.py
@@ -1,10 +1,14 @@
 import sys
 import argparse
 
+
 def main():
   parser = argparse.ArgumentParser()
+  parser.add_argument('key', type=int)
   parser.add_argument('filename', nargs='?', help='name of input file')
   args = parser.parse_args()
+
+  print(args.key)
   if(args.filename):
     inf = open(args.filename)
   else:
