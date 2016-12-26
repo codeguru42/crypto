eed458195b2c97084cefebf5575f1903016db239
Code Apprentice <codeguru42@gmail.com>
2016-12-26 16:04:38 -0700

    Use loadTestsFromModule() instead of loadTestsFromTestCase()

--- alltests.py
+++ alltests.py
@@ -10,10 +10,10 @@ import viginere
 def suite():
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
-    suite.addTest(loader.loadTestsFromTestCase(s1c1.TestHexToBase64))
-    suite.addTest(loader.loadTestsFromTestCase(s1c2.TestXor))
-    suite.addTest(loader.loadTestsFromTestCase(s1c3.TestOneByteXor))
-    suite.addTest(loader.loadTestsFromTestCase(s1c4.TestBreakAll))
+    suite.addTest(loader.loadTestsFromModule(s1c1))
+    suite.addTest(loader.loadTestsFromModule(s1c2))
+    suite.addTest(loader.loadTestsFromModule(s1c3))
+    suite.addTest(loader.loadTestsFromModule(s1c4))
     suite.addTest(loader.loadTestsFromModule(viginere))
 
     return suite
