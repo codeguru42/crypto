fe828f7385230e8b0b3d5790716e1a1fe1c8ed10
Code Apprentice <codeguru42@gmail.com>
2016-12-25 19:28:00 -0700

    Viginere cipher

---
+++ viginere.py
@@ -0,0 +1,52 @@
+import argparse
+
+import sys
+
+import itertools
+
+
+def viginere(key, text):
+    def char_to_int(c):
+        return ord(c) - ord('A')
+
+    def str_to_ints(s):
+        return list(char_to_int(x) for x in s.upper())
+
+    key_int = str_to_ints(key)
+    text_int = str_to_ints(text)
+    cipher = [map(lambda x, y: (x + y) % 26, key_int, text_group) for text_group in grouper(text_int, len(key_int))]
+    cipher = ((chr(c + ord('A')) for c in x) for x in cipher)
+    return ''.join(''.join(x) for x in cipher)
+
+
+def break_viginere(cipher):
+    pass
+
+
+def grouper(iterable, n):
+    return itertools.zip_longest(*([iter(iterable)] * n), fillvalue=0)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-k', '--key', help='encryption key')
+    group.add_argument('-b', '--break-cipher', help='break encryption', action='store_true')
+    parser.add_argument('filename', nargs='?', help='name of input file')
+    args = parser.parse_args()
+
+    if args.filename:
+        inf = open(args.filename)
+    else:
+        inf = sys.stdin
+
+    for line in inf:
+        if args.break_cipher:
+            break_viginere(line.strip())
+        else:
+            print('viginere:', viginere(args.key, line.strip()))
+    inf.close()
+
+
+if __name__ == "__main__":
+    main()
