3b3d3975739f06e65aaf7bb636fd7b736215e129
Code Apprentice <codeguru42@gmail.com>
2016-12-30 17:18:15 -0700

    Set 1 Challenge 6: Implement hamming_distance()

--- s1c6.py
+++ s1c6.py
@@ -1,12 +1,20 @@
 import unittest
+import bitstring
+
+import cryptopals
 
 
 class TestHammingDistance(unittest.TestCase):
     def testCase(self):
-        str1 = 'this is a test'
-        str2 = 'wokka wokka!!!'
+        str1 = b'this is a test'
+        str2 = b'wokka wokka!!!'
         expected = 37
         self.assertEqual(expected, hamming_distance(str1, str2))
+
+
+def hamming_distance(str1, str2):
+    temp = cryptopals.xor(str1, str2)
+    return sum(bitstring.Bits(temp))
 
 
 if __name__ == "__main__":
