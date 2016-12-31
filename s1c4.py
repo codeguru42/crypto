443f4914213c73123ad48294033881dc2c58ee0b
Code Apprentice <codeguru42@gmail.com>
2016-12-30 19:45:46 -0700

    Refactor s1c4.py and fix a bug missed in the original main()

--- s1c4.py
+++ s1c4.py
@@ -9,17 +9,17 @@ class TestBreakAll(unittest.TestCase):
     def testCase(self):
         filename = 's1c4.txt'
         expected = b'nOW\x00THAT\x00THE\x00PARTY\x00IS\x00JUMPING*'
-        with open(filename) as file:
-            plaintexts = map(lambda line: cryptopals.break_xor(bytes.fromhex(line.strip()), 1), file)
-            best = min(plaintexts, key=cryptopals.distance_from_english)
-            self.assertEqual(expected, best)
+        self.assertEqual(expected, break_file(filename))
+
+
+def break_file(filename):
+    with open(filename) as file:
+        plaintexts = map(lambda line: cryptopals.break_xor(bytes.fromhex(line.strip()), 1), file)
+        return min(plaintexts, key=cryptopals.distance_from_english)
 
 
 def main():
-    with open(sys.argv[1]) as file:
-        plaintexts = map(lambda line: cryptopals.break_xor(bytes.fromhex(line.strip())), file)
-        best = min(plaintexts, key=cryptopals.distance_from_english)
-        print(repr(best))
+    print(break_file(sys.argv[1]))
 
 
 if __name__ == "__main__":
