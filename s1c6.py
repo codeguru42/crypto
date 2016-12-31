637af86420604f684030d76a93bb240c86fd2958
Code Apprentice <codeguru42@gmail.com>
2016-12-30 21:33:41 -0700

    Set 1 Challenge 6: Calculate possible key length

--- s1c6.py
+++ s1c6.py
@@ -1,4 +1,7 @@
+import base64
+import sys
 import unittest
+
 import bitstring
 
 import cryptopals
@@ -17,5 +20,19 @@ def hamming_distance(str1, str2):
     return sum(bitstring.Bits(temp))
 
 
+def normalized_edit_distance(text, keylen):
+    dist = hamming_distance(text[:keylen], text[keylen:2 * keylen])
+    return dist / keylen
+
+
+def best_keylen(filename):
+    with open(filename) as file:
+        first_line = base64.b64decode(file.readline())
+        return min(range(2, 40), key=lambda l: normalized_edit_distance(first_line, l))
+
+
+def main():
+    print(best_keylen(sys.argv[1]))
+
 if __name__ == "__main__":
-    unittest.main()
+    main()
