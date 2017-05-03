44cadf8ccf7f5723778a8852eb111766419293f1
Code Apprentice <codeguru42@gmail.com>
2017-05-02 18:28:00 -0600

    Set 1 Challenge 8: Print out any line which has at least one repeating block

--- s1c8.py
+++ s1c8.py
@@ -6,11 +6,16 @@ from cryptopals import grouper
 
 def main():
     with open(argv[1]) as file:
-        for line in file:
+        for line_num, line in enumerate(file):
             data = bytes.fromhex(line.strip())
-            print('data:', data)
             block_count = Counter(grouper(data, 16))
-            print('blocks:', block_count)
+            is_aes = False
+            for block in block_count:
+                if block_count[block] > 1:
+                    is_aes = True
+            if is_aes:
+                print("Line", line_num)
+                print(data)
 
 
 if __name__ == "__main__":
