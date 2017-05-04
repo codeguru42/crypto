2bf7d74c3d410c7e199460a7a213e49d6ecd82cd
Code Apprentice <codeguru42@gmail.com>
2017-05-04 17:41:16 -0600

    Set 2 Challenge 10: Stub for solution

---
+++ set2/c10.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestCbcEncrypt(unittest.TestCase):
+    def testCase(self):
+        pass
+
+
+def cbc_encrypt(key, text, iv):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
