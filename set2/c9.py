f032cb9ec454cc81b0e60185385b4b9b17d972be
Code Apprentice <codeguru42@gmail.com>
2017-05-03 20:14:44 -0600

    Set 2 Challenge 9: Implement pkcs7()

--- set2/c9.py
+++ set2/c9.py
@@ -5,12 +5,13 @@ class TestPkcs7(unittest.TestCase):
     def testCase(self):
         text = b'YELLOW SUBMARINE'
         expected = b'YELLOW SUBMARINE\x04\x04\x04\x04'
-        actual = pkcs7(text)
+        actual = pkcs7(text, 20)
         self.assertEqual(expected, actual)
 
 
-def pkcs7(text):
-    pass
+def pkcs7(text, block_length):
+    padding = block_length - len(text)
+    return text + bytes([padding] * padding)
 
 
 if __name__ == '__main__':
