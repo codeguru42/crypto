da5cd71a11fd9af04bacf09e5bdb23cb06aff2fa
Code Apprentice <codeguru42@gmail.com>
2017-05-03 19:59:49 -0600

    Set 2 Challenge 9: Write test case

--- set2/c9.py
+++ set2/c9.py
@@ -3,7 +3,10 @@ import unittest
 
 class TestPkcs7(unittest.TestCase):
     def testCase(self):
-        pass
+        text = b'YELLOW SUBMARINE'
+        expected = b'YELLOW SUBMARINE\x04\x04\x04\x04'
+        actual = pkcs7(text)
+        self.assertEqual(expected, actual)
 
 
 def pkcs7(text):
