db5e407e6630f2164e9b82e07c589ae2ebd0c20e
Code Apprentice <codeguru42@gmail.com>
2017-05-04 18:08:05 -0600

    Set 2 Challenge 10: Implement CBC encryption

--- set2/c10.py
+++ set2/c10.py
@@ -1,4 +1,6 @@
 import unittest
+from Cryptodome.Cipher import AES
+from cryptopals import grouper, xor
 
 
 class TestCbcEncrypt(unittest.TestCase):
@@ -7,11 +9,18 @@ class TestCbcEncrypt(unittest.TestCase):
         iv = b'\x00' * 16
         text = b'The quick red fox jumped over the lazy brown dog'
         cipher_text = cbc_encrypt(key, text, iv)
+        print(cipher_text)
         self.assertEqual(text, cbc_decrypt(key, cipher_text, iv))
 
 
 def cbc_encrypt(key, text, iv):
-    pass
+    aes = AES.new(key, AES.MODE_ECB)
+    cipher_text = []
+    data = iv
+    for block in grouper(text, 16):
+        data = xor(data, bytes(block))
+        cipher_text.append(aes.encrypt(data))
+    return b''.join(cipher_text)
 
 
 def cbc_decrypt(key, text, iv):
