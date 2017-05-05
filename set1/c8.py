a8c6eed1b092a06e7e571bd87c230da54168f3fb
Code Apprentice <codeguru42@gmail.com>
2017-05-04 20:04:37 -0600

    Set 2 Challenge 11: Refactor code to detect ECB into a function

--- set1/c8.py
+++ set1/c8.py
@@ -4,16 +4,20 @@ from sys import argv
 from cryptopals import grouper
 
 
+def is_ecb(cipher):
+    block_count = Counter(grouper(cipher, 16))
+    ecb = False
+    for block in block_count:
+        if block_count[block] > 1:
+            ecb = True
+    return ecb
+
+
 def main():
     with open(argv[1]) as file:
         for line_num, line in enumerate(file):
             data = bytes.fromhex(line.strip())
-            block_count = Counter(grouper(data, 16))
-            is_aes = False
-            for block in block_count:
-                if block_count[block] > 1:
-                    is_aes = True
-            if is_aes:
+            if is_ecb(data):
                 print("Line", line_num + 1)
                 print(data)
 
