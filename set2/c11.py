0931c69f86a12ec2b3df7b4b4a8f331fe44e2de2
Code Apprentice <codeguru42@gmail.com>
2017-05-04 20:42:44 -0600

    Set 2 Challenge 11: Add main() to test the random key generator

--- set2/c11.py
+++ set2/c11.py
@@ -3,4 +3,13 @@ import secrets
 
 def random_key(keylen):
     rng = secrets.SystemRandom()
-    return rng.sample(xrange(256), keylen)
+    return bytes(rng.sample(range(256), keylen))
+
+
+def main():
+    for i in range(10):
+        print(random_key(16))
+
+
+if __name__ == '__main__':
+    main()
