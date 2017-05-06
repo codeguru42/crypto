dda05f46a69a9771a35b3b50186900d522876983
Code Apprentice <codeguru42@gmail.com>
2017-05-06 11:16:31 -0600

    Set 2 Challenge 11: Implement encrypt_with_random_key()

--- set2/c11.py
+++ set2/c11.py
@@ -1,14 +1,37 @@
 import secrets
 
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad
 
-def random_key(keylen):
+
+def random_bytes(byte_count):
     rng = secrets.SystemRandom()
-    return bytes(rng.choices(range(256), k=keylen))
+    return bytes(rng.choices(range(256), k=byte_count))
+
+
+def encrypt_with_random_key(plain):
+    key_length = 16
+    block_size = 16
+    rng = secrets.SystemRandom()
+    key = random_bytes(key_length)
+    mode = rng.randrange(1, 3)
+    iv = random_bytes(block_size)
+
+    if mode == AES.MODE_ECB:
+        aes = AES.new(key, mode)
+    else:
+        aes = AES.new(key, mode, iv=iv)
+
+    prefix = random_bytes(rng.randrange(5, 11))
+    suffix = random_bytes(rng.randrange(5, 11))
+    plain = pad(prefix + plain + suffix, block_size)
+    return aes.encrypt(plain)
 
 
 def main():
     for i in range(10):
-        print(random_key(16))
+        text = b'The quick red fox jumped over the lazy brown dog.'
+        print(encrypt_with_random_key(text))
 
 
 if __name__ == '__main__':
